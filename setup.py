3eeb1ea2fb7902f2b0757f36e1263e5126cbdd9a
Pedro Ramaciotti <pedroramaciotti@users.noreply.github.com>
2022-01-30 16:15:29 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
+
 from distutils.core import setup
 
 setup(
   name = 'linate',         
-  packages = ['Language-Independent Network Attitudinal Embedding'],   
-  version = '0.1',      
+  version = '0.1.0',      
   license='MIT',        
-  description = 'A Python module to embed social networks in attitudinal opinion spaces',   
-  author = 'Pedro Ramaciotti Morales',                  
-  author_email = 'pedro.ramaciotti@gmail.com',     
-  url = 'https://pedroramaciotti.github.io',   
-  download_url = 'https://github.com/pedroramaciotti/linate/archive/v_0.1.tar.gz',
+  description = 'Language-Independent Network Attitudinal Embedding',   
+  author = 'Pedro Ramaciotti Morales',                      
+  url = 'https://github.com/pedroramaciotti/linate',   
   keywords = ['graph embedding', 'correnpondence analysis', 'social network analysis'],
   install_requires=[            
           'numpy',
           'pandas',
           'scipy',
       ],
+  packages = ["linate"],
+  include_package_data=True,
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
