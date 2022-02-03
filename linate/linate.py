83c2973b1e8e0e193225457c0c0f924f4e80d03e
Foula <foula.vagena@gmail.com>
2022-02-03 12:51:15 +0100

    CA class

--- linate/linate.py
+++ linate/linate.py
@@ -57,7 +57,7 @@ class LINATE(BaseEstimator, TransformerMixin):
             raise BaseException('Twitter network file one of first two columns should be the \'source\' attribute') 
         ntwrk_df.dropna(subset = [self.target_col_name, self.source_col_name], inplace = True)
 
-        print(ntwrk_df.shape)
+        #print(ntwrk_df.shape)
         if in_nd_thrshold > 0:
             degree_per_target = ntwrk_df.groupby(self.target_col_name).count()
             if self.multiplicity_col_name in degree_per_target.columns:
@@ -65,7 +65,7 @@ class LINATE(BaseEstimator, TransformerMixin):
             degree_per_target = degree_per_target[degree_per_target > in_nd_thrshold].dropna().reset_index()
             degree_per_target.drop(self.source_col_name, axis = 1, inplace = True)
             ntwrk_df = pd.merge(ntwrk_df, degree_per_target, on = [self.target_col_name], how = 'inner')
-        print(ntwrk_df.shape)
+        #print(ntwrk_df.shape)
 
         if out_nd_thrshold > 0:
             degree_per_source = ntwrk_df.groupby(self.source_col_name).count()
@@ -77,18 +77,23 @@ class LINATE(BaseEstimator, TransformerMixin):
 
         # TODO: what should we be doing witht he multiplicity column?
         ntwrk_df.drop(self.multiplicity_col_name, axis = 1, inplace = True)
-        print(ntwrk_df.shape)
+        #print(ntwrk_df.shape)
 
         n_i, r = ntwrk_df[self.source_col_name].factorize()
-        source_users_no = len(np.unique(n_i))
+        self.source_users_no_ = len(np.unique(n_i))
+        self.column_ids_ = r.values
+        #print(source_users_no, len(n_i), len(r))
         n_j, c = ntwrk_df[self.target_col_name].factorize()
         assert len(n_i) == len(n_j)
-        target_no = len(np.unique(n_j))
+        self.target_users_no_ = len(np.unique(n_j))
+        self.row_ids_ = c.values
+        #print(target_users_no, len(n_j), len(c))
         network_edge_no = len(n_i)
         n_in_j, tups = pd.factorize(list(zip(n_j, n_i)))
         ntwrk_csr = csr_matrix((np.bincount(n_in_j), tuple(zip(*tups))))
         # TODO : add delayed sparse?
         ntwrk_np = ntwrk_csr.toarray()
+        #print(ntwrk_np.shape)
 
         ca_model = ca_randomized = prince.CA(n_components = self.n_components, n_iter = self.n_iter,
                 copy = self.copy, check_input = self.check_input, engine = self.engine, random_state = self.random_state)
--- test_linate_class.py
+++ test_linate_class.py
@@ -6,7 +6,8 @@ def main():
     model = LINATE(n_components = 2, out_degree_threshold = 2, in_degree_threshold = 2, engine = 'fbpca')
     model.fit('data/twitter_networks/Austria_20201027_MPs_followers_tids.csv')
 
-    print(type(model.eigenvalues_), type(model.total_inertia_), type(model.explained_inertia_))
+    #print(len(model.ca_row_coordinates_), len(model.row_ids_), 
+     #       len(model.ca_column_coordinates_), len(model.column_ids_))
 
 if __name__ == "__main__":
     main()
