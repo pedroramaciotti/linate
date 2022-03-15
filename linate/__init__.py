ef2f2c8471a1e234008469ce1a92e6b33af3ad72
Foula <foula.vagena@gmail.com>
2022-03-15 12:30:40 +0100

    CA

--- linate/__init__.py
+++ linate/__init__.py
@@ -1,5 +1,7 @@
 from .ca import CA
 
+from .affine_transformation import AffineTransformation
+
 from .__version__ import __version__
 
-__all__ = ['CA', '__version__']
+__all__ = ['CA', '__version__', 'AffineTransformation']
