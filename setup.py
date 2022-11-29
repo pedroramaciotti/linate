fe38fe3c4a619ea1e34c428ef2463f5e3fae1d84
Pedro Ramaciotti <pedroramaciotti@users.noreply.github.com>
2022-11-29 19:25:42 +0100

    version release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
   name = 'linate',         
-  version = '0.3',      
+  version = '0.4',      
   license='MIT',        
   description = 'Language-Independent Network Attitudinal Embedding',   
   author = 'Pedro Ramaciotti Morales',                      
